f4da801f32dd0c3c15f5623c0b6520f0267579d3
AlpizarGregory <gregory.ac@estudiantec.cr>
2025-06-22 00:27:53 -0600

    Basic file operations
    - Added read, write, open and release operations.

--- read_write_cdev/hello_cdev.c
+++ read_write_cdev/hello_cdev.c
@@ -5,11 +5,45 @@
 #define DEVICE_NAME "hello_cdev"
 
 static int major;
+static char text[64];
 
-static ssize_t my_read(struct file *f, char __user *u, size_t l, loff_t *o)
+static ssize_t my_read(struct file *filp, char __user *user_buf, size_t len, loff_t *off)
 {
-    pr_info("hello_cdev - Read is called\n");
-    return 0;
+    int not_copied, delta, to_copy = (len + *off) < sizeof(text) ? len : (sizeof(text) - *off);
+
+    pr_info("hello_cdev - Read is called, we want to read %ld bytes, but actually only copying %d bytes. The offset is %lld\n", len, to_copy, *off);
+
+    if (*off >= sizeof(text))
+        return 0;
+
+    not_copied = copy_to_user(user_buf, &text[*off], to_copy);
+    delta = to_copy - not_copied;
+
+    if (not_copied)
+        pr_warn("hello_cdev - Could only copy %d bytes\n", delta);
+
+    *off += delta;
+
+    return delta;
+}
+
+static ssize_t my_write(struct file *filp, const char __user *user_buf, size_t len, loff_t *off)
+{
+    int not_copied, delta, to_copy = (len + *off) < sizeof(text) ? len : (sizeof(text) - *off);
+
+    pr_info("hello_cdev - Write is called, we want to read %ld bytes, but actually only copying %d bytes. The offset is %lld\n", len, to_copy, *off);
+
+    if (*off >= sizeof(text))
+        return 0;
+
+    not_copied = copy_from_user(&text[*off], user_buf, to_copy);
+    delta = to_copy - not_copied;
+
+    if (not_copied)
+        pr_warn("hello_cdev - Could only copy %d bytes\n", delta);
+
+    *off += delta;
+    return delta;
 }
 
 static int my_open(struct inode *inode, struct file *filp)
@@ -34,6 +68,7 @@ static struct file_operations fops = {
     .read = my_read,
     .open = my_open,
     .release = my_release,
+    .write = my_write,
 };
 
 static int __init my_init(void)
